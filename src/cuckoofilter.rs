dac644b64876592228def62ff1cff9062c338ec5
Marco Neumann <marco@crepererum.net>
2018-06-24 13:25:01 +0200

    cuckoofilter: variable fingerprint size

--- src/cuckoofilter.rs
+++ src/cuckoofilter.rs
@@ -2,14 +2,11 @@
 use std::collections::hash_map::DefaultHasher;
 use std::fmt;
 use std::hash::{BuildHasher, Hash, Hasher};
-use std::mem;
 
+use fixedbitset::FixedBitSet;
 use rand::Rng;
 
 use hash_utils::MyBuildHasherDefault;
-
-// TODO: make fingerprint type a type param of CuckooFilter
-type Fingerprint = u8;
 
 const MAX_NUM_KICKS: usize = 500; // mentioned in paper
 
@@ -25,11 +22,12 @@ where
     R: Rng,
     B: BuildHasher + Clone + Eq,
 {
-    table: Vec<Fingerprint>,
+    table: FixedBitSet,
     n_elements: usize,
     buildhasher: B,
     bucketsize: usize,
     n_buckets: usize,
+    l_fingerprint: usize,
     rng: R,
 }
 
@@ -42,11 +40,12 @@ where
     /// - `rng`: random number generator used for certain random actions
     /// - `bucketsize`: number of elements per bucket, must be at least 2
     /// - `n_buckets`: number of buckets, must be a power of 2 and at least 2
+    /// - `l_fingerprint`: size of the fingerprint in bits
     ///
     /// The BuildHasher is set to the `DefaultHasher`.
-    pub fn with_params(rng: R, bucketsize: usize, n_buckets: usize) -> Self {
+    pub fn with_params(rng: R, bucketsize: usize, n_buckets: usize, l_fingerprint: usize) -> Self {
         let bh = MyBuildHasherDefault::<DefaultHasher>::default();
-        Self::with_params_and_hash(rng, bucketsize, n_buckets, bh)
+        Self::with_params_and_hash(rng, bucketsize, n_buckets, l_fingerprint, bh)
     }
 }
 
@@ -60,12 +59,19 @@ where
     /// - `rng`: random number generator used for certain random actions
     /// - `bucketsize`: number of elements per bucket, must be at least 2
     /// - `n_buckets`: number of buckets, must be a power of 2 and at least 2
+    /// - `l_fingerprint`: size of the fingerprint in bits
     /// - `bh`: BuildHasher that creates Hash objects, used for fingerprint creation and
     ///   fingerprint hashing
-    pub fn with_params_and_hash(rng: R, bucketsize: usize, n_buckets: usize, bh: B) -> Self {
+    pub fn with_params_and_hash(
+        rng: R,
+        bucketsize: usize,
+        n_buckets: usize,
+        l_fingerprint: usize,
+        bh: B,
+    ) -> Self {
         assert!(
             bucketsize >= 2,
-            "bucketsize ({}) should be greater or equal than 2",
+            "bucketsize ({}) must be greater or equal than 2",
             bucketsize
         );
         assert!(
@@ -73,13 +79,25 @@ where
             "n_buckets ({}) must be a power of 2 and greater or equal than 2",
             n_buckets
         );
+        assert!(
+            (l_fingerprint > 1) & (l_fingerprint <= 64),
+            "l_fingerprint ({}) must be greater than 1 and less or equal than 64",
+            l_fingerprint
+        );
+
+        let table_size = n_buckets
+            .checked_mul(bucketsize)
+            .expect("Table size too large")
+            .checked_mul(l_fingerprint)
+            .expect("Table size too large");
 
         Self {
-            table: vec![0; n_buckets * bucketsize],
+            table: FixedBitSet::with_capacity(table_size),
             n_elements: 0,
             buildhasher: bh,
             bucketsize: bucketsize,
             n_buckets: n_buckets,
+            l_fingerprint: l_fingerprint,
             rng: rng,
         }
     }
@@ -92,6 +110,11 @@ where
     /// Number of buckets used by the CuckooFilter.
     pub fn n_buckets(&self) -> usize {
         self.n_buckets
+    }
+
+    /// Size of the used fingerprint in bits
+    pub fn l_fingerprint(&self) -> usize {
+        self.l_fingerprint
     }
 
     /// Check if CuckooFilter is empty, i.e. contains no elements.
@@ -136,7 +159,11 @@ where
             let offset = i * self.bucketsize;
             let x = offset + e;
 
-            mem::swap(&mut self.table[x], &mut f);
+            // swap table[x] and f
+            let tmp = self.table_read(x);
+            self.table_write(x, f);
+            f = tmp;
+
             i = i ^ self.hash(&f);
             if self.write_to_bucket(i, f) {
                 self.n_elements += 1;
@@ -186,7 +213,7 @@ where
         false
     }
 
-    fn start<T>(&self, t: &T) -> (Fingerprint, usize, usize)
+    fn start<T>(&self, t: &T) -> (u64, usize, usize)
     where
         T: Hash,
     {
@@ -196,7 +223,7 @@ where
         (f, i1, i2)
     }
 
-    fn fingerprint<T>(&self, t: &T) -> Fingerprint
+    fn fingerprint<T>(&self, t: &T) -> u64
     where
         T: Hash,
     {
@@ -205,7 +232,12 @@ where
         t.hash(&mut hasher);
 
         // don't produce 0, since this is used as "free"-slot value
-        1 + (hasher.finish() % (Fingerprint::max_value() as u64 - 1)) as Fingerprint
+        let x_mod = if self.l_fingerprint == 64 {
+            u64::max_value()
+        } else {
+            (1u64 << self.l_fingerprint) - 1
+        };
+        1 + (hasher.finish() % x_mod)
     }
 
     fn hash<T>(&self, t: &T) -> usize
@@ -218,36 +250,53 @@ where
         (hasher.finish() & (self.n_buckets as u64 - 1)) as usize
     }
 
-    fn write_to_bucket(&mut self, i: usize, f: Fingerprint) -> bool {
+    fn write_to_bucket(&mut self, i: usize, f: u64) -> bool {
         let offset = i * self.bucketsize;
         for x in offset..(offset + self.bucketsize) {
-            if self.table[x] == 0 {
-                self.table[x] = f;
+            if self.table_read(x) == 0 {
+                self.table_write(x, f);
                 return true;
             }
         }
         false
     }
 
-    fn has_in_bucket(&self, i: usize, f: Fingerprint) -> bool {
+    fn has_in_bucket(&self, i: usize, f: u64) -> bool {
         let offset = i * self.bucketsize;
         for x in offset..(offset + self.bucketsize) {
-            if self.table[x] == f {
+            if self.table_read(x) == f {
                 return true;
             }
         }
         false
     }
 
-    fn remove_from_bucket(&mut self, i: usize, f: Fingerprint) -> bool {
+    fn remove_from_bucket(&mut self, i: usize, f: u64) -> bool {
         let offset = i * self.bucketsize;
         for x in offset..(offset + self.bucketsize) {
-            if self.table[x] == f {
-                self.table[x] = 0;
+            if self.table_read(x) == f {
+                self.table_write(x, 0);
                 return true;
             }
         }
         false
+    }
+
+    fn table_read(&self, i: usize) -> u64 {
+        let offset = i * self.l_fingerprint;
+        let mut result = 0u64;
+        for b in 0..self.l_fingerprint {
+            result |= (self.table[offset + b] as u64) << (self.l_fingerprint - b - 1);
+        }
+        result
+    }
+
+    fn table_write(&mut self, i: usize, f: u64) {
+        let offset = i * self.l_fingerprint;
+        for b in 0..self.l_fingerprint {
+            self.table
+                .set(offset + b, (f >> (self.l_fingerprint - b - 1) & 1u64) != 0);
+        }
     }
 }
 
@@ -271,52 +320,101 @@ mod tests {
     use rand::{ChaChaRng, SeedableRng};
 
     #[test]
-    #[should_panic(expected = "bucketsize (0) should be greater or equal than 2")]
+    #[should_panic(expected = "bucketsize (0) must be greater or equal than 2")]
     fn new_panics_bucketsize_0() {
-        CuckooFilter::with_params(ChaChaRng::from_seed([0; 32]), 0, 16);
+        CuckooFilter::with_params(ChaChaRng::from_seed([0; 32]), 0, 16, 8);
     }
 
     #[test]
-    #[should_panic(expected = "bucketsize (1) should be greater or equal than 2")]
+    #[should_panic(expected = "bucketsize (1) must be greater or equal than 2")]
     fn new_panics_bucketsize_1() {
-        CuckooFilter::with_params(ChaChaRng::from_seed([0; 32]), 1, 16);
+        CuckooFilter::with_params(ChaChaRng::from_seed([0; 32]), 1, 16, 8);
     }
 
     #[test]
     #[should_panic(expected = "n_buckets (0) must be a power of 2 and greater or equal than 2")]
     fn new_panics_n_buckets_0() {
-        CuckooFilter::with_params(ChaChaRng::from_seed([0; 32]), 2, 0);
+        CuckooFilter::with_params(ChaChaRng::from_seed([0; 32]), 2, 0, 8);
     }
 
     #[test]
     #[should_panic(expected = "n_buckets (1) must be a power of 2 and greater or equal than 2")]
     fn new_panics_n_buckets_1() {
-        CuckooFilter::with_params(ChaChaRng::from_seed([0; 32]), 2, 1);
+        CuckooFilter::with_params(ChaChaRng::from_seed([0; 32]), 2, 1, 8);
     }
 
     #[test]
     #[should_panic(expected = "n_buckets (5) must be a power of 2 and greater or equal than 2")]
     fn new_panics_n_buckets_5() {
-        CuckooFilter::with_params(ChaChaRng::from_seed([0; 32]), 2, 5);
+        CuckooFilter::with_params(ChaChaRng::from_seed([0; 32]), 2, 5, 8);
+    }
+
+    #[test]
+    #[should_panic(expected = "l_fingerprint (0) must be greater than 1 and less or equal than 64")]
+    fn new_panics_l_fingerprint_0() {
+        CuckooFilter::with_params(ChaChaRng::from_seed([0; 32]), 2, 16, 0);
+    }
+
+    #[test]
+    #[should_panic(expected = "l_fingerprint (1) must be greater than 1 and less or equal than 64")]
+    fn new_panics_l_fingerprint_1() {
+        CuckooFilter::with_params(ChaChaRng::from_seed([0; 32]), 2, 16, 1);
+    }
+
+    #[test]
+    #[should_panic(
+        expected = "l_fingerprint (65) must be greater than 1 and less or equal than 64"
+    )]
+    fn new_panics_l_fingerprint_65() {
+        CuckooFilter::with_params(ChaChaRng::from_seed([0; 32]), 2, 16, 65);
+    }
+
+    #[test]
+    #[should_panic(expected = "Table size too large")]
+    fn new_panics_table_size_overflow_1() {
+        CuckooFilter::with_params(ChaChaRng::from_seed([0; 32]), usize::max_value(), 2, 2);
+    }
+
+    #[test]
+    #[should_panic(expected = "Table size too large")]
+    fn new_panics_table_size_overflow_2() {
+        CuckooFilter::with_params(
+            ChaChaRng::from_seed([0; 32]),
+            2,
+            (((usize::max_value() as u128) + 1) / 2) as usize,
+            2,
+        );
+    }
+
+    #[test]
+    #[should_panic(expected = "Table size too large")]
+    fn new_panics_table_size_overflow_3() {
+        CuckooFilter::with_params(
+            ChaChaRng::from_seed([0; 32]),
+            2,
+            (((usize::max_value() as u128) + 1) / 8) as usize,
+            64,
+        );
     }
 
     #[test]
     fn getter() {
-        let cf = CuckooFilter::with_params(ChaChaRng::from_seed([0; 32]), 2, 16);
+        let cf = CuckooFilter::with_params(ChaChaRng::from_seed([0; 32]), 2, 16, 8);
         assert_eq!(cf.bucketsize(), 2);
         assert_eq!(cf.n_buckets(), 16);
+        assert_eq!(cf.l_fingerprint(), 8);
     }
 
     #[test]
     fn empty() {
-        let cf = CuckooFilter::with_params(ChaChaRng::from_seed([0; 32]), 2, 16);
+        let cf = CuckooFilter::with_params(ChaChaRng::from_seed([0; 32]), 2, 16, 8);
         assert!(cf.empty());
         assert_eq!(cf.len(), 0);
     }
 
     #[test]
     fn insert() {
-        let mut cf = CuckooFilter::with_params(ChaChaRng::from_seed([0; 32]), 2, 16);
+        let mut cf = CuckooFilter::with_params(ChaChaRng::from_seed([0; 32]), 2, 16, 8);
         cf.insert(&13).unwrap();
         assert!(!cf.empty());
         assert_eq!(cf.len(), 1);
@@ -326,7 +424,7 @@ mod tests {
 
     #[test]
     fn delete() {
-        let mut cf = CuckooFilter::with_params(ChaChaRng::from_seed([0; 32]), 2, 16);
+        let mut cf = CuckooFilter::with_params(ChaChaRng::from_seed([0; 32]), 2, 16, 8);
         cf.insert(&13).unwrap();
         cf.insert(&42).unwrap();
         assert!(cf.lookup(&13));
@@ -341,7 +439,7 @@ mod tests {
 
     #[test]
     fn full() {
-        let mut cf = CuckooFilter::with_params(ChaChaRng::from_seed([0; 32]), 2, 2);
+        let mut cf = CuckooFilter::with_params(ChaChaRng::from_seed([0; 32]), 2, 2, 8);
 
         for i in 0..4 {
             cf.insert(&i).unwrap();
@@ -358,7 +456,7 @@ mod tests {
 
     #[test]
     fn debug() {
-        let cf = CuckooFilter::with_params(ChaChaRng::from_seed([0; 32]), 2, 16);
+        let cf = CuckooFilter::with_params(ChaChaRng::from_seed([0; 32]), 2, 16, 8);
         assert_eq!(
             format!("{:?}", cf),
             "CuckooFilter { bucketsize: 2, n_buckets: 16 }"
@@ -367,7 +465,7 @@ mod tests {
 
     #[test]
     fn clone() {
-        let mut cf1 = CuckooFilter::with_params(ChaChaRng::from_seed([0; 32]), 2, 16);
+        let mut cf1 = CuckooFilter::with_params(ChaChaRng::from_seed([0; 32]), 2, 16, 8);
         cf1.insert(&13).unwrap();
         assert!(cf1.lookup(&13));
 
