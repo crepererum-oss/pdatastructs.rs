8d270cc3cbb6e8de58d1fd8b6873859ff2ca8c2a
Marco Neumann <marco@crepererum.net>
2018-11-09 08:24:21 +0100

    filters: extend benachmarks

--- benches/filters.rs
+++ benches/filters.rs
@@ -4,7 +4,7 @@ extern crate pdatastructs;
 
 use std::collections::HashSet;
 
-use criterion::{Bencher, Criterion, Fun};
+use criterion::{Bencher, Criterion, Fun, ParameterizedBenchmark};
 use pdatastructs::filters::bloomfilter::BloomFilter;
 use pdatastructs::filters::cuckoofilter::CuckooFilter;
 use pdatastructs::filters::quotientfilter::QuotientFilter;
@@ -13,13 +13,13 @@ use pdatastructs::rand::{ChaChaRng, SeedableRng};
 
 fn setup_bloomfilter() -> BloomFilter<u64> {
     let false_positive_rate = 0.02; // = 2%
-    let expected_elements = 1_000_000;
+    let expected_elements = 10_000;
     BloomFilter::with_properties(expected_elements, false_positive_rate)
 }
 
 fn setup_cuckoofilter() -> CuckooFilter<u64, ChaChaRng> {
     let false_positive_rate = 0.02; // = 2%
-    let expected_elements = 1_000_000;
+    let expected_elements = 10_000;
     let rng = ChaChaRng::from_seed([0; 32]);
     CuckooFilter::with_properties_8(false_positive_rate, expected_elements, rng)
 }
@@ -29,7 +29,7 @@ fn setup_hashset() -> HashSet<u64> {
 }
 
 fn setup_quotientfilter() -> QuotientFilter<u64> {
-    let bits_quotient = 24;
+    let bits_quotient = 15;
     let bits_remainder = 4;
     QuotientFilter::with_params(bits_quotient, bits_remainder)
 }
@@ -65,19 +65,18 @@ fn benchmarks_setup(c: &mut Criterion) {
 }
 
 fn benchmarks_insert_many(c: &mut Criterion) {
-    let functions = vec![
-        Fun::new("bloomfilter", |b, n| {
-            run_insert_many(setup_bloomfilter, b, *n)
-        }),
-        Fun::new("cuckoofilter", |b, n| {
-            run_insert_many(setup_cuckoofilter, b, *n)
-        }),
-        Fun::new("hashset", |b, n| run_insert_many(setup_hashset, b, *n)),
-        Fun::new("quotientfilter", |b, n| {
-            run_insert_many(setup_quotientfilter, b, *n)
-        }),
-    ];
-    c.bench_functions("insert_many", functions, 100);
+    let parameters = vec![4_000, 8_000, 12_000, 16_000, 20_000];
+    let benchmark = ParameterizedBenchmark::new(
+        "bloomfilter",
+        |b, n| run_insert_many(setup_bloomfilter, b, *n),
+        parameters,
+    ).with_function("cuckoofilter", |b, n| {
+        run_insert_many(setup_cuckoofilter, b, *n)
+    }).with_function("hashset", |b, n| run_insert_many(setup_hashset, b, *n))
+    .with_function("quotientfilter", |b, n| {
+        run_insert_many(setup_quotientfilter, b, *n)
+    }).sample_size(20);
+    c.bench("insert_many", benchmark);
 }
 
 criterion_group!(benches, benchmarks_setup, benchmarks_insert_many,);
