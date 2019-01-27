4a5ab43ab992d59b0568b0d34f561a9c766a95a0
Marco Neumann <marco@crepererum.net>
2019-01-27 18:34:32 +0100

    tdigest: docs

--- src/tdigest.rs
+++ src/tdigest.rs
@@ -1,4 +1,4 @@
-//! TODO
+//! TDigest implementation.
 use std::cell::RefCell;
 use std::f64;
 
@@ -204,16 +204,281 @@ impl TDigestInner {
     }
 }
 
-/// TODO
+/// A TDigest is a data structure to capture quantile and CDF (cumulative distribution function)
+/// data for arbitrary distribution w/o the need to store the entire data and w/o requiring the
+/// user to know ranges beforehand. It can handle outliers and data w/ non-uniform densities (like
+/// mixed gaussian data w/ very different deviations).
 ///
-/// https://github.com/tdunning/t-digest/blob/master/docs/t-digest-paper/histo.pdf
+/// # Examples
+/// ```
+/// use pdatastructs::tdigest::TDigest;
+/// use rand::{Rng, SeedableRng};
+/// use rand::distributions::StandardNormal;
+/// use rand_chacha::ChaChaRng;
+///
+/// // Set up moderately compressed digest
+/// let compression_factor = 100.;
+/// let max_backlog_size = 10;
+/// let mut digest = TDigest::new(compression_factor, max_backlog_size);
+///
+/// // sample data from normal distribution
+/// let mut rng = ChaChaRng::from_seed([0; 32]);
+/// let n = 100_000;
+/// for _ in 0..n {
+///     let x = rng.sample(StandardNormal);
+///     digest.insert(x);
+/// }
+///
+/// // test some known quantiles
+/// assert!((-1.2816 - digest.quantile(0.10)).abs() < 0.01);
+/// assert!((-0.6745 - digest.quantile(0.25)).abs() < 0.01);
+/// assert!((0.0000 - digest.quantile(0.5)).abs() < 0.01);
+/// assert!((0.6745 - digest.quantile(0.75)).abs() < 0.01);
+/// assert!((1.2816 - digest.quantile(0.90)).abs() < 0.01);
+/// ```
+/// # Applications
+/// - capturing distribution information in big data applications
+/// - gathering quantiles for monitoring purposes (e.g. quantiles of web application response times)
+///
+/// # How It Works
+/// Initially, the digest is empty and has no centroids stored.
+///
+/// ## Insertion
+/// If a new element is presented to the digest, a temporary centroid w/ the given weight and
+/// position is created and added to a backlog. If the backlog reaches a certain size, it is merged
+/// into the digest.
+///
+/// ## Merge Procedure
+/// For the merge, all centroids (the existing ones and the ones in the backlog) are added to a
+/// single list and sorted by position (i.e. their mean which is `sum / count`). Then, the list is
+/// processed in linear order. Every centroid is merged w/ the next one if together they would stay
+/// under a certain size limit.
+///
+/// The size limit is determined by the compression factor and the total weight added to the
+/// filter. They simplest thing is to use a uniform size limit for all centroids (also known as k0
+/// scale function), which is equivalent to `\delta / 2 * q` (`\delta` being the compression factor
+/// and `q` being the position of the centroid in the sorted list ranging from 0 to 1). The
+/// following plot shows k0 w/ a compression factor of 10:
+///
+/// ```text
+///   5 +---------------------------------------------------+
+///     |         +          +         +          +    **** |
+///     |                                           ****    |
+///   4 |-+                                      ****     +-|
+///     |                                     ***           |
+///     |                                 ****              |
+///   3 |-+                            ****               +-|
+///     |                           ***                     |
+///     |                        ***                        |
+///     |                     ***                           |
+///   2 |-+               ****                            +-|
+///     |              ****                                 |
+///     |           ***                                     |
+///   1 |-+     ****                                      +-|
+///     |    ****                                           |
+///     | ****    +          +         +          +         |
+///   0 +---------------------------------------------------+
+///     0        0.2        0.4       0.6        0.8        1
+/// ```
+///
+/// The problem here is that outlier can have a high influence on the guessed distribution. To
+/// account for this, there is another scale function k1 which is equivalent to
+/// `\delta / (2 * PI) * \asin(2q - 1)`. The following plot shows this function for a compression
+/// factor of 10:
+///
+/// ```text
+///   3 +---------------------------------------------------+
+///     |         +          +         +          +         |
+///     |                                                  *|
+///   2 |-+                                              ***|
+///     |                                            ****   |
+///   1 |-+                                      *****    +-|
+///     |                                  ******           |
+///     |                            *******                |
+///   0 |-+                    *******                    +-|
+///     |                *******                            |
+///     |           ******                                  |
+///  -1 |-+    *****                                      +-|
+///     |   ****                                            |
+///  -2 |***                                              +-|
+///     |*                                                  |
+///     |         +          +         +          +         |
+///  -3 +---------------------------------------------------+
+///     0        0.2        0.4       0.6        0.8        1
+/// ```
+///
+/// ## Querying
+/// To query quantiles or CDF values, the sorted list of centroids is scanned until the right
+/// centroid is found. The data between two consecutive centroids is interpolated in a linear
+/// fashion. The same holds for the first and the last centroid where an interpolation with the
+/// measured minimum and maximum of the data takes place.
+///
+/// ## Example
+/// Imagine the following example of mixed gaussian distribution:
+///
+/// - 30% μ=-1.9 σ=0.2
+/// - 70% μ=0.7 σ=0.8
+///
+/// This would lead to the following probability density function (PDF):
+///
+/// ```text
+/// 0.6 +-------------------------------------------------+
+///     |       +**      +       +       +        +       |
+///     |        **                                       |
+/// 0.5 |-+     * *                                     +-|
+///     |       *  *                                      |
+/// 0.4 |-+     *  *                                    +-|
+///     |       *  *                                      |
+///     |       *  *                ******                |
+/// 0.3 |-+    *   *               **     **            +-|
+///     |      *    *            **        **             |
+///     |      *    *           **           *            |
+/// 0.2 |-+    *    *          **             *         +-|
+///     |      *    *         **               **         |
+/// 0.1 |-+   *     *       **                  **      +-|
+///     |     *      *    ***                     **      |
+///     |    *  +    ******      +       +        + ***   |
+///   0 +-------------------------------------------------+
+///    -3      -2       -1       0       1        2       3
+/// ```
+///
+/// and to the following cumulative distribution function (CDF):
+///
+/// ```text
+///   1 +-------------------------------------------------+
+///     |       +        +       +       +    *****       |
+/// 0.9 |-+                                 ***         +-|
+/// 0.8 |-+                               ***           +-|
+///     |                                **               |
+/// 0.7 |-+                            **               +-|
+/// 0.6 |-+                           **                +-|
+///     |                           **                    |
+/// 0.5 |-+                       ***                   +-|
+///     |                       ***                       |
+/// 0.4 |-+                  ****                       +-|
+/// 0.3 |-+         *********                           +-|
+///     |          **                                     |
+/// 0.2 |-+       *                                     +-|
+/// 0.1 |-+      *                                      +-|
+///     |      **        +       +       +        +       |
+///   0 +-------------------------------------------------+
+///    -3      -2       -1       0       1        2       3
+/// ```
+///
+/// Extreme compression (factor is 10), resulting in 8 centroids:
+///
+/// ```text
+///   1 +-------------------------------------------------+
+///     |       +        +       +       +       *+       |
+/// 0.9 |-+                                ******       +-|
+/// 0.8 |-+                                *            +-|
+///     |                                 *               |
+/// 0.7 |-+                           *****             +-|
+/// 0.6 |-+                           *                 +-|
+///     |                            *                    |
+/// 0.5 |-+                    *******                  +-|
+///     |                      *                          |
+/// 0.4 |-+                    *                        +-|
+/// 0.3 |-+                   *                         +-|
+///     |           ***********                           |
+/// 0.2 |-+      ***                                    +-|
+/// 0.1 |-+      *                                      +-|
+///     |       *        +       +       +        +       |
+///   0 +-------------------------------------------------+
+///    -3      -2       -1       0       1        2       3
+/// ```
+///
+/// High compression (factor is 20), resulting in 15 centroids:
+///
+/// ```text
+///   1 +-------------------------------------------------+
+///     |       +        +       +       +    *** +       |
+/// 0.9 |-+                                ****         +-|
+/// 0.8 |-+                                *            +-|
+///     |                                **               |
+/// 0.7 |-+                              *              +-|
+/// 0.6 |-+                          ****               +-|
+///     |                            *                    |
+/// 0.5 |-+                       ***                   +-|
+///     |                         *                       |
+/// 0.4 |-+                *******                      +-|
+/// 0.3 |-+                *                            +-|
+///     |           *******                               |
+/// 0.2 |-+       **                                    +-|
+/// 0.1 |-+      *                                      +-|
+///     |       *        +       +       +        +       |
+///   0 +-------------------------------------------------+
+///    -3      -2       -1       0       1        2       3
+/// ```
+///
+/// Medium compression (factor is 50), resulting in 34 centroids:
+///
+/// ```text
+///   1 +-------------------------------------------------+
+///     |       +        +       +       +    ****+       |
+/// 0.9 |-+                                 ***         +-|
+/// 0.8 |-+                                *            +-|
+///     |                                **               |
+/// 0.7 |-+                             *               +-|
+/// 0.6 |-+                          ***                +-|
+///     |                           *                     |
+/// 0.5 |-+                        **                   +-|
+///     |                         *                       |
+/// 0.4 |-+                 ******                      +-|
+/// 0.3 |-+            *****                            +-|
+///     |          ****                                   |
+/// 0.2 |-+       *                                     +-|
+/// 0.1 |-+     **                                      +-|
+///     |       *        +       +       +        +       |
+///   0 +-------------------------------------------------+
+///    -3      -2       -1       0       1        2       3
+/// ```
+///
+/// Low compression (factor is 200), resulting in 127 centroids:
+///
+/// ```text
+///   1 +-------------------------------------------------+
+///     |       +        +       +       +    *****       |
+/// 0.9 |-+                                  **         +-|
+/// 0.8 |-+                               ***           +-|
+///     |                                *                |
+/// 0.7 |-+                             **              +-|
+/// 0.6 |-+                           **                +-|
+///     |                           **                    |
+/// 0.5 |-+                        **                   +-|
+///     |                        **                       |
+/// 0.4 |-+                  ****                       +-|
+/// 0.3 |-+          ********                           +-|
+///     |          **                                     |
+/// 0.2 |-+       *                                     +-|
+/// 0.1 |-+      *                                      +-|
+///     |       *        +       +       +        +       |
+///   0 +-------------------------------------------------+
+///    -3      -2       -1       0       1        2       3
+/// ```
+///
+/// # See Also
+/// - `pdatastructs::reservoirsampling::ReservoirSampling`: Less complex data structure that (in
+///   case of a high sampling rate) can also be used to capture distribution information
+///
+/// # References
+/// - ["Computing Extremely Accurate Quantiles using t-Digests", T. Dunning, O. Ertl, 2018](https://github.com/tdunning/t-digest/blob/master/docs/t-digest-paper/histo.pdf)
+/// - [Python Implementation, C. Davidson-pilon, MIT License](https://github.com/CamDavidsonPilon/tdigest)
+/// - [Go Implementation, InfluxData, Apache License 2.0](https://github.com/influxdata/tdigest)
 #[derive(Debug)]
 pub struct TDigest {
     inner: RefCell<TDigestInner>,
 }
 
 impl TDigest {
-    /// TODO
+    /// Create a new, empty TDigest w/ the following parameters:
+    ///
+    /// - `compression_factor`: how many centroids (relative to the added weight) should be kept,
+    ///   i.e. higher factors mean more precise distribution information but less performance and
+    ///   higher memory requirements.
+    /// - `max_backlog_size`: maximum number of centroids to keep in a backlog before starting a
+    ///   merge, i.e. higher numbers mean higher insertion performance but higher temporary memory
+    ///   requirements.
     pub fn new(compression_factor: f64, max_backlog_size: usize) -> Self {
         assert!(
             (compression_factor > 1.) && compression_factor.is_finite(),
@@ -226,12 +491,12 @@ impl TDigest {
         }
     }
 
-    /// TODO
+    /// Get compression factor of the TDigest.
     pub fn compression_factor(&self) -> f64 {
         self.inner.borrow().compression_factor
     }
 
-    /// TODO
+    /// Get the maximum number of centroids to be stored in the backlog before starting a merge.
     pub fn max_backlog_size(&self) -> usize {
         self.inner.borrow().max_backlog_size
     }
@@ -249,12 +514,18 @@ impl TDigest {
         self.inner.borrow().centroids.len()
     }
 
-    /// TODO
+    /// Insert new element into the TDigest.
+    ///
+    /// The implicit weight of the element is 1.
+    ///
+    /// Panics if the element is not finite.
     pub fn insert(&mut self, x: f64) {
         self.insert_weighted(x, 1.);
     }
 
-    /// TODO
+    /// Insert a new element into the TDigest w/ the given weight.
+    ///
+    /// Panics if the element is not finite or if the weight is negative.
     pub fn insert_weighted(&mut self, x: f64, w: f64) {
         assert!(x.is_finite(), "x ({}) must be finite", x);
         assert!(
@@ -271,7 +542,13 @@ impl TDigest {
         self.inner.borrow_mut().insert_weighted(x, w)
     }
 
-    /// TODO
+    /// Queries the quantile of the TDigest.
+    ///
+    /// If the digest is empty, NaN will be returned.
+    ///
+    /// If there are any unmerged centroids in the backlog, this will trigger a merge process.
+    ///
+    /// Panics if the quantile is not in range `[0, 1]`.
     pub fn quantile(&self, q: f64) -> f64 {
         assert!((q >= 0.) && (q <= 1.), "q ({}) must be in [0, 1]", q);
 
@@ -282,8 +559,16 @@ impl TDigest {
         self.inner.borrow().quantile(q)
     }
 
-    /// TODO
+    /// Queries the cumulative distribution function (CDF) of the TDigest.
+    ///
+    /// If the digest is empty, 0 will be returned.
+    ///
+    /// If there are any unmerged centroids in the backlog, this will trigger a merge process.
+    ///
+    /// Panics if the given value is NaN.
     pub fn cdf(&self, x: f64) -> f64 {
+        assert!(!x.is_nan(), "x must not be NaN");
+
         // apply compression if required
         self.inner.borrow_mut().merge();
 
@@ -291,7 +576,11 @@ impl TDigest {
         self.inner.borrow().cdf(x)
     }
 
-    /// TODO
+    /// Give the number of elements in the digest.
+    ///
+    /// If weighted elements were used, this corresponds to the sum of all weights.
+    ///
+    /// If the digest is empty, 0 will be returned.
     pub fn count(&self) -> f64 {
         // apply compression if required
         self.inner.borrow_mut().merge();
@@ -300,7 +589,9 @@ impl TDigest {
         self.inner.borrow().count()
     }
 
-    /// TODO
+    /// Give the (weighted) sum of all elements inserted into the digest.
+    ///
+    /// If the digest is empty, 0 will be returned.
     pub fn sum(&self) -> f64 {
         // apply compression if required
         self.inner.borrow_mut().merge();
@@ -309,7 +600,9 @@ impl TDigest {
         self.inner.borrow().sum()
     }
 
-    /// TODO
+    /// Give the (weighted) mean of all elements inserted into the digest.
+    ///
+    /// If the digest is empty, NaN will be returned.
     pub fn mean(&self) -> f64 {
         // apply compression if required
         self.inner.borrow_mut().merge();
@@ -318,12 +611,16 @@ impl TDigest {
         inner.sum() / inner.count()
     }
 
-    /// TODO
+    /// Give the minimum of all elements inserted into the digest.
+    ///
+    /// If the digest is empty, +Inf will be returned.
     pub fn min(&self) -> f64 {
         self.inner.borrow().min
     }
 
-    /// TODO
+    /// Give the maximum of all elements inserted into the digest.
+    ///
+    /// If the digest is empty, -Inf will be returned.
     pub fn max(&self) -> f64 {
         self.inner.borrow().max
     }
@@ -688,5 +985,14 @@ mod tests {
         let max_backlog_size = 13;
         let mut digest = TDigest::new(compression_factor, max_backlog_size);
         digest.insert(f64::NAN);
+    }
+
+    #[test]
+    #[should_panic(expected = "x must not be NaN")]
+    fn invalid_cdf() {
+        let compression_factor = 2.;
+        let max_backlog_size = 13;
+        let digest = TDigest::new(compression_factor, max_backlog_size);
+        digest.cdf(f64::NAN);
     }
 }
