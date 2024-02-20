73816654b5e4b5fe690f35dcc47ad301d24d6538
Miroslav Zoricak <1315417+mirosval@users.noreply.github.com>
2024-02-20 21:53:15 +0100

    implement serde directly

--- src/hyperloglog.rs
+++ src/hyperloglog.rs
@@ -331,9 +331,10 @@ where
     }
 }
 
-impl<T> fmt::Debug for HyperLogLog<T>
+impl<T, B> fmt::Debug for HyperLogLog<T, B>
 where
     T: Hash + ?Sized,
+    B: BuildHasher + Clone + Eq,
 {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "HyperLogLog {{ b: {} }}", self.b)
@@ -363,91 +364,200 @@ where
 }
 
 /// Serde support for `pdatastructs::hyperloglog::HyperLogLog`
-///
-/// # Examples
-/// ```
-/// use pdatastructs::hyperloglog::HyperLogLog;
-/// use pdatastructs::hyperloglog::serde::HyperLogLogExport;
-/// use serde_json;
-/// use std::hash::BuildHasherDefault;
-/// use std::collections::hash_map::DefaultHasher;
-///
-/// let mut hll = HyperLogLog::new(4);
-/// hll.add(&"some string");
-///
-/// let hll_export = HyperLogLogExport::from(&hll);
-/// let json = serde_json::to_string(&hll_export).expect("valid json");
-///
-/// // elsewhere
-/// let hll_export = serde_json::from_str(&json).expect("valid export");
-/// let hll: HyperLogLog<str, BuildHasherDefault<DefaultHasher>> = HyperLogLog::from_exported_hyperloglog(hll_export, BuildHasherDefault::default());
-///
-/// assert!(hll.count() > 0);
-/// ```
 #[cfg(feature = "serde")]
 pub mod serde {
-
     use super::HyperLogLog;
+    use serde::de::{self, Visitor};
+    use serde::ser::SerializeStruct;
     use serde::{Deserialize, Serialize};
     use std::fmt;
     use std::hash::{BuildHasher, Hash};
     use std::marker::PhantomData;
 
-    impl<T, B> HyperLogLog<T, B>
+    impl<T, B> Serialize for HyperLogLog<T, B>
     where
         T: Hash + ?Sized,
-        B: BuildHasher + Clone + Eq,
+        B: BuildHasher + Clone + Eq + Serialize,
     {
-        /// Reconstruct HyperLogLog from a previously exported HyperLogLog
-        pub fn from_exported_hyperloglog(exported: HyperLogLogExport, buildhasher: B) -> Self {
-            Self {
-                registers: exported.registers,
-                b: exported.b,
-                buildhasher,
-                phantom: PhantomData,
-            }
+        fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: serde::Serializer,
+        {
+            let mut state = serializer.serialize_struct("HyperLogLog", 3)?;
+            state.serialize_field("registers", &self.registers)?;
+            state.serialize_field("b", &self.b)?;
+            state.serialize_field("buildhasher", &self.buildhasher)?;
+            state.end()
         }
     }
 
-    /// Captures the underlying HyperLogLog data such that it can be serialized and deserialized
-    /// using serde
-    #[derive(Clone, Serialize, Deserialize, Eq, PartialEq)]
-    pub struct HyperLogLogExport {
-        /// HLL Data
-        pub registers: Vec<u8>,
-        /// Number of bits used for register selection
-        pub b: usize,
-    }
-
-    impl<T, H> From<HyperLogLog<T, H>> for HyperLogLogExport
+    impl<'de, T, B> Deserialize<'de> for HyperLogLog<T, B>
     where
         T: Hash + ?Sized,
-        H: BuildHasher + Clone + Eq,
+        B: BuildHasher + Clone + Eq + Deserialize<'de>,
     {
-        fn from(value: HyperLogLog<T, H>) -> Self {
-            Self {
-                registers: value.registers,
-                b: value.b,
+        fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where
+            D: serde::Deserializer<'de>,
+        {
+            enum Field {
+                Registers,
+                B,
+                Buildhasher,
             }
+            impl<'de> Deserialize<'de> for Field {
+                fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+                where
+                    D: serde::Deserializer<'de>,
+                {
+                    struct FieldVisitor;
+                    impl<'de> Visitor<'de> for FieldVisitor {
+                        type Value = Field;
+
+                        fn expecting(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
+                            formatter.write_str("`registers` or `b` or `buildhasher")
+                        }
+
+                        fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+                        where
+                            E: de::Error,
+                        {
+                            match v {
+                                "registers" => Ok(Field::Registers),
+                                "b" => Ok(Field::B),
+                                "buildhasher" => Ok(Field::Buildhasher),
+                                _ => Err(de::Error::unknown_field(v, FIELDS)),
+                            }
+                        }
+                    }
+
+                    deserializer.deserialize_identifier(FieldVisitor)
+                }
+            }
+            struct HyperLogLogVisitor<T, B>
+            where
+                T: Hash + ?Sized,
+                B: BuildHasher + Clone + Eq,
+            {
+                _t: PhantomData<T>,
+                _b: PhantomData<B>,
+            }
+            impl<T, B> HyperLogLogVisitor<T, B>
+            where
+                T: Hash + ?Sized,
+                B: BuildHasher + Clone + Eq,
+            {
+                fn new() -> Self {
+                    Self {
+                        _t: PhantomData::default(),
+                        _b: PhantomData::default(),
+                    }
+                }
+            }
+            impl<'de, T, B> Visitor<'de> for HyperLogLogVisitor<T, B>
+            where
+                T: Hash + ?Sized,
+                B: BuildHasher + Clone + Eq + Deserialize<'de>,
+            {
+                type Value = HyperLogLog<T, B>;
+
+                fn expecting(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
+                    formatter.write_str("struct HyperLogLog")
+                }
+
+                fn visit_map<A>(self, mut map: A) -> Result<Self::Value, A::Error>
+                where
+                    A: serde::de::MapAccess<'de>,
+                {
+                    let mut registers = None;
+                    let mut b = None;
+                    let mut buildhasher = None;
+                    while let Some(key) = map.next_key()? {
+                        match key {
+                            Field::Registers => {
+                                if registers.is_some() {
+                                    return Err(de::Error::duplicate_field("registers"));
+                                }
+                                registers = Some(map.next_value()?);
+                            }
+                            Field::B => {
+                                if b.is_some() {
+                                    return Err(de::Error::duplicate_field("b"));
+                                }
+                                b = Some(map.next_value()?);
+                            }
+                            Field::Buildhasher => {
+                                if buildhasher.is_some() {
+                                    return Err(de::Error::duplicate_field("buildhasher"));
+                                }
+                                buildhasher = Some(map.next_value()?);
+                            }
+                        }
+                    }
+                    let registers =
+                        registers.ok_or_else(|| de::Error::missing_field("registers"))?;
+                    let b = b.ok_or_else(|| de::Error::missing_field("b"))?;
+                    let buildhasher =
+                        buildhasher.ok_or_else(|| de::Error::missing_field("buildhasher"))?;
+                    Ok(HyperLogLog {
+                        registers,
+                        b,
+                        buildhasher,
+                        phantom: PhantomData::default(),
+                    })
+                }
+            }
+            const FIELDS: &'static [&'static str] = &["registers", "b", "buildhasher"];
+            deserializer.deserialize_struct(
+                "HyperLogLog",
+                FIELDS,
+                HyperLogLogVisitor::<T, B>::new(),
+            )
         }
     }
 
-    impl<T, H> From<&HyperLogLog<T, H>> for HyperLogLogExport
-    where
-        T: Hash + ?Sized,
-        H: BuildHasher + Clone + Eq,
-    {
-        fn from(value: &HyperLogLog<T, H>) -> Self {
-            Self {
-                registers: value.registers.clone(),
-                b: value.b,
-            }
-        }
-    }
+    #[cfg(test)]
+    mod tests {
+        use std::hash::{BuildHasher, Hasher};
 
-    impl fmt::Debug for HyperLogLogExport {
-        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-            write!(f, "HyperLogLogExport {{ b: {} }}", self.b)
+        use serde::{Deserialize, Serialize};
+
+        use crate::hyperloglog::HyperLogLog;
+
+        #[test]
+        fn serde() {
+            #[derive(Clone, Debug, Serialize, Deserialize, Eq, PartialEq)]
+            struct MyHasher {
+                state: u64,
+            }
+            impl Hasher for MyHasher {
+                fn finish(&self) -> u64 {
+                    self.state
+                }
+
+                fn write(&mut self, bytes: &[u8]) {
+                    let _ = bytes;
+                }
+            }
+            impl BuildHasher for MyHasher {
+                type Hasher = MyHasher;
+                fn build_hasher(&self) -> Self::Hasher {
+                    MyHasher { state: 4 }
+                }
+            }
+            let hasher = MyHasher { state: 4 };
+            // Construct a HLL
+            let mut hll = HyperLogLog::with_hash(4, hasher.clone());
+            hll.add("abc");
+            // Serialize to JSON
+            let json = serde_json::to_string(&hll).expect("can serialize to json");
+            // Deserialize back to HLL
+            let mut de_hll = serde_json::from_str(&json).expect("can deserialize from json");
+            // Check they're the same
+            assert_eq!(hll, de_hll);
+            // Add the same string again to check if the hasher is reconstructed correctly
+            de_hll.add("abc");
+            assert_eq!(hll, de_hll);
         }
     }
 }
@@ -456,10 +566,8 @@ pub mod serde {
 mod tests {
     use super::HyperLogLog;
     use crate::hash_utils::BuildHasherSeeded;
-    use crate::hyperloglog::serde::HyperLogLogExport;
     use crate::hyperloglog_data::{RAW_ESTIMATE_DATA_OFFSET, RAW_ESTIMATE_DATA_VEC};
     use crate::test_util::{assert_send, NotSend};
-    use std::hash::BuildHasherDefault;
 
     #[test]
     #[should_panic(expected = "b (3) must be larger or equal than 4 and smaller or equal than 18")]
@@ -814,18 +922,5 @@ mod tests {
     fn send() {
         let hll: HyperLogLog<NotSend> = HyperLogLog::new(4);
         assert_send(&hll);
-    }
-
-    #[test]
-    fn serde() {
-        let hasher = BuildHasherDefault::default();
-        let mut hll = HyperLogLog::with_hash(4, hasher.clone());
-        hll.add("abc");
-        let export: HyperLogLogExport = HyperLogLogExport::from(&hll);
-        let json = serde_json::to_string(&export).expect("can serialize to json");
-        let de_hll_export = serde_json::from_str(&json).expect("can deserialize from json");
-        let de_hll: HyperLogLog<str, _> =
-            HyperLogLog::from_exported_hyperloglog(de_hll_export, hasher);
-        assert_eq!(hll, de_hll);
     }
 }
